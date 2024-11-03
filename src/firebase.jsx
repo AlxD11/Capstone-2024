a50673220a2922e8b38dc61bc5926e230ade5466
Vishnu Varshan Krishna Kumar <vishnuvarshankrishnakumar@my.unt.edu>
2024-11-02 20:45:39 -0500

    vite config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@ import firebase from  'firebase/compat/app'
 import 'firebase/compat/auth' 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAZkt_KHfc1Wnbcjx-cs66-7dwISsRRP8A",
-  authDomain: "test-website-4aef6.firebaseapp.com",
-  projectId: "test-website-4aef6",
-  storageBucket: "test-website-4aef6.firebasestorage.app",
-  messagingSenderId: "421926823892",
-  appId: "1:421926823892:web:5c57d0fbaa6371779d9841",
-  measurementId: "G-R8J2CQJD6N"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()
