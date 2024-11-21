e1a1a4291dd1c349aba9b42d1ad84bd3487de6d5
Alex D <alexdierks@protonmail.com>
2024-11-21 09:19:12 -0600

    Added a visual distinction between the top navigation bar and the main body content. Not sure if I like it.

--- src/components/MoodPollScreen.jsx
+++ src/components/MoodPollScreen.jsx
@@ -1,4 +1,3 @@
-import appLogo from '../assets/app_logo.png';
 import smile_excellent from '../assets/smile_excellent.png';
 import smile_good from '../assets/smile_good.png';
 import smile_neutral from '../assets/smile_neutral.png';
@@ -11,33 +10,108 @@ import Footer from './Footer.jsx'
 import NavBar from './NavBar.jsx'
 
 
+function PollQuestionSleep()
+{
+	return(
+		<div className="PollQuestion">
+			<p>How's your sleep quality?</p>
+			<ul>
+				<li>
+					<span>Excellent</span>
+					<img src={smile_excellent} className="poll-icon" alt="excellent" />
+				</li>
+				<li>
+					<span>Good</span>
+					<img src={smile_good} className="poll-icon" alt="good" />
+				</li>
+				<li>
+					<span>So-so</span>
+					<img src={smile_neutral} className="poll-icon" alt="neutral" />
+				</li>
+				<li>
+					<span>Poor</span>
+					<img src={smile_poor} className="poll-icon" alt="poor" />
+				</li>
+				<li>
+					<span>Horrible</span>
+					<img src={smile_miserable} className="poll-icon" alt="miserable" />
+				</li>
+			</ul>
+		</div>	
+	);
+}
+
+function PollQuestionPhysicalEnergy()
+{
+	return(
+		<div className="PollQuestion">
+			<p>How's your physical energy level?</p>
+			<ul>
+				<li>
+					<span>Excellent</span>
+					<img src={smile_excellent} className="poll-icon" alt="excellent" />
+				</li>
+				<li>
+					<span>Good</span>
+					<img src={smile_good} className="poll-icon" alt="good" />
+				</li>
+				<li>
+					<span>So-so</span>
+					<img src={smile_neutral} className="poll-icon" alt="neutral" />
+				</li>
+				<li>
+					<span>Poor</span>
+					<img src={smile_poor} className="poll-icon" alt="poor" />
+				</li>
+				<li>
+					<span>Horrible</span>
+					<img src={smile_miserable} className="poll-icon" alt="miserable" />
+				</li>
+			</ul>
+		</div>	
+	);
+}
+
+function PollQuestionMentalEnergy()
+{
+	return(
+		<div className="PollQuestion">
+			<p>How's your mental energy level?</p>
+			<ul>
+				<li>
+					<span>Excellent</span>
+					<img src={smile_excellent} className="poll-icon" alt="excellent" />
+				</li>
+				<li>
+					<span>Good</span>
+					<img src={smile_good} className="poll-icon" alt="good" />
+				</li>
+				<li>
+					<span>So-so</span>
+					<img src={smile_neutral} className="poll-icon" alt="neutral" />
+				</li>
+				<li>
+					<span>Poor</span>
+					<img src={smile_poor} className="poll-icon" alt="poor" />
+				</li>
+				<li>
+					<span>Horrible</span>
+					<img src={smile_miserable} className="poll-icon" alt="miserable" />
+				</li>
+			</ul>
+		</div>	
+	);
+}
+
 function MoodPoll()
 {
 	return (
 		<div className="MoodPoll">
 			<h2>Mood Poll</h2>
-			<p>1 of 5: How would you rate your sleep quality?</p>
-			<ul>
-				<li>Excellent
-					<img src={smile_excellent} className="poll-icon" alt="excellent" />
-				</li>
-				<li>
-					Good
-					<img src={smile_good} className="poll-icon" alt="good" />
-				</li>
-				<li>
-					So-so
-					<img src={smile_neutral} className="poll-icon" alt="neutral" />
-				</li>
-				<li>
-					Poor
-					<img src={smile_poor} className="poll-icon" alt="poor" />
-				</li>
-				<li>
-					Horrible
-					<img src={smile_miserable} className="poll-icon" alt="miserable" />
-				</li>
-			</ul>
+			<p>Let's see how you've been doing lately.</p>
+			<PollQuestionSleep />
+			<PollQuestionPhysicalEnergy />
+			<PollQuestionMentalEnergy />
 		</div>
 	);
 }
