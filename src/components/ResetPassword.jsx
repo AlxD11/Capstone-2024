c27f16333df9bac09e31e2fafb3d7b0e6cece1f4
Salma <salmaomary@my.unt.edu>
2024-11-24 15:50:50 -0600

    Started reset password component, created variables and handleSubmit func

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,10 +1,38 @@
 // src/components/ResetPassword.js
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuth } from "../contexts/AuthContext";
+import { Alert } from "react-bootstrap";
 
 function ResetPassword() {
+
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { resetPassword } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setMessage('');
+    setLoading(true);
+
+    try {
+      await resetPassword(email);
+      setMessage('Check your inbox for further instructions.');
+    } catch {
+      setError('Failed to reset password. Please try again.');
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div style={styles.container}>
       <h2>Reset Password</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
+      {message && <Alert variant="success">{message}</Alert>}
       <p>&lt;to be edited&gt;</p>
     </div>
   );
