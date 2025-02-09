b3dd46651381f938e43864b24e676b15d0dc7fb5
AlxD11 <alexdierks@protonmail.com>
2025-02-09 13:11:28 -0600

    Edit: Made MainScreen a reusable component for other pages (to avoid re-creating the navigation and footer on each page)

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,16 +1,22 @@
 import Footer from './Footer.jsx'
 import NavBar from './NavBar.jsx'
-import HomeScreen from './HomeScreen.jsx'
 
-function MainScreen()
+/** Displays a typical user-facing page and includes the top navigation bar and standard footer.
+ * 
+ * @param {Object} param0 
+ * @param {React.JSX} param0.children 	The component to render in the main window.
+ */
+function MainScreen({ children })
 {
 	return(
 		<>
 			<NavBar />
-			<HomeScreen/>
+				<div className="MainScreen-content">
+					{children}
+				</div>
 			<Footer />
 		</>
 	);
 }
 
-export default MainScreen
+export default MainScreen;
