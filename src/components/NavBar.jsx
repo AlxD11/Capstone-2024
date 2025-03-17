128c10f9d5cdd5da569794373b9a9f2e1dedecc1
Deastrocdor <143763381+Deastrocdor@users.noreply.github.com>
2025-03-17 09:54:23 -0500

    Update NavBar.jsx
    updated navbar to include dropdown menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,18 @@
 import appLogo from '../assets/app_logo.png';
 import ProfilePicture from './ProfilePicture';
-import { Link } from 'react-router-dom'
-import '../styles/GlobalStyles.css'
+import { Link, useNavigate } from 'react-router-dom';
+import '../styles/GlobalStyles.css';
+import { useState } from 'react';
 
 function NavBar() {
-	const photoURL = ProfilePicture();
+	const [showDropdown, setShowDropdown] = useState(false);
+	const navigate = useNavigate();
+
+	const handleDropdownToggle = () => {
+		setShowDropdown((prev) => !prev);
+	};
+
+
 	return (
 		<div className="NavBar">
 			<Link to="/home">
@@ -12,16 +20,34 @@ function NavBar() {
 			</Link>
 
 			<nav>
-				<Link to ="/home">Home</Link>
-				<Link to ="/mood-journal">Mood Journal</Link>
-				<Link to="/Reports">Report</Link>
+				<Link to="/home">Home</Link>
+				<Link to="/mood-journal">Mood Journal</Link>
+				<Link to="/reports">Report</Link>
 			</nav>
 
-			<Link to="/profile">
-				<img src={photoURL} className="avatar" alt="User avatar" />
-			</Link>
+			<div onClick={handleDropdownToggle} className="avatar-wrapper">
+				<ProfilePicture />
+			</div>
+
+			{showDropdown && (
+				<div className="p-4 w-52 shadow-lg absolute -left-14 top-24">
+					<ul>
+						<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
+							<Link to="/profile-page">Profile Settings</Link>
+						</li>
+						<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
+							<Link to="/application-settings">Application Settings</Link>
+						</li>
+						<li
+							className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100"
+						>
+							Logout
+						</li>
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
 
-export default NavBar
+export default NavBar;
