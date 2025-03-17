830e21272e0124b3d2679c3886d6ae9bd2f494c3
Deastrocdor <143763381+Deastrocdor@users.noreply.github.com>
2025-03-17 10:07:26 -0500

    Update NavBar.jsx
    added absolute tags and (hopefully) fixed linking issue

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,13 @@ import { useState } from 'react';
 
 function NavBar() {
 	const [showDropdown, setShowDropdown] = useState(false);
+	
 	const navigate = useNavigate();
 
 	const handleDropdownToggle = () => {
 		setShowDropdown((prev) => !prev);
 	};
-
+	
 	const photoURL = ProfilePicture();
 
 	return (
@@ -26,27 +27,27 @@ function NavBar() {
 				<Link to="/reports">Report</Link>
 			</nav>
 
-			<div onClick={handleDropdownToggle} className="avatar-wrapper">
+			<div className="avatar-wrapper relative" onClick={handleDropdownToggle}>
 				<img src={photoURL} className="avatar" alt="User avatar" />
-			</div>
 
-			{showDropdown && (
-				<div className="p-4 w-52 shadow-lg absolute -left-14 top-24">
-					<ul>
-						<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
-							<Link to="/profile-page">Profile Settings</Link>
-						</li>
-						<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
-							<Link to="/application-settings">Application Settings</Link>
-						</li>
-						<li
-							className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100"
-						>
-							Logout
-						</li>
-					</ul>
-				</div>
-			)}
+				{showDropdown && (
+					<div className="dropdown-menu p-4 w-52 shadow-lg absolute left-0 top-full mt-2">
+						<ul>
+							<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
+								<Link to="/profile-page">Profile Settings</Link>
+							</li>
+							<li className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100">
+								<Link to="/application-settings-page">Application Settings</Link>
+							</li>
+							<li
+								className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100"
+							>
+								Logout
+							</li>
+						</ul>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
