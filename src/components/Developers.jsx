121705ce53dc1b92d8c5a075c3550404a477a400
Salma Omary <salmamomary1@gmail.com>
2025-03-31 22:46:52 -0500

    Complete Developers.jsx
    this file includes the description and the developers information (our emails)

--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,39 +1,36 @@
 import '../styles/GlobalStyles.css';
 import MainScreen from './MainScreen.jsx';
-import { Link, useNavigate } from 'react-router-dom';
 
-function Func({ link })
-{
-	<div>
-		<p> link is <a href={link}>here</a>. (link)</p>
-	</div>
-}
-function Developers()
-{
-	
-	return (
-		<div className="Developers">
-			<h2>Resources</h2>
-			<p>Put all your cool stuff here</p>
-			
-			<Func link="https://duckduckgo.com/?t=ffab&q=BroCode+React+cards+tutorial+-noai&iax=videos&ia=videos&iai=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DyYiwxYqQ9vg" />
-		</div>
-	);
-}
+function Developers() {
+    return (
+        <div className="Developers">
+            <h2>Meet the Developers</h2>
+            <p>
+                This website was developed by a group of students from the University of North Texas (UNT) as part of our Senior Capstone Project. Below are the developers who contributed to this project:
+            </p>
 
-// These last two things are what this file "returns."
-
-
-function DevelopersPage(){
-	/* Important: React components MUST return only one element.
-	Either wrap everything in a <div>, or use blank tags like this does. */
-	return (
-		<>
-			<MainScreen>
-				<Developers />
-			</MainScreen>
-		</>
-	);
+            <div className="developer-section">
+                <h3>🧑‍💻 Developers</h3>
+                <ul>
+                    <li><strong>Alex Dierks:</strong> <a href="mailto:alexdierks@my.unt.edu">alexdierks@my.unt.edu</a></li>
+					<li><strong>Rohan James:</strong> <a href="mailto:rohanjames@my.unt.edu">rohanjames@my.unt.edu</a></li>
+					<li><strong>Vishnu Kumar:</strong> <a href="mailto:vishnuvarshankrishnakumar@my.unt.edu">vishnuvarshankrishnakumar@my.unt.edu</a></li>
+                    <li><strong>Salma Omary:</strong> <a href="mailto:salmaomary@my.unt.edu">salmaomary@my.unt.edu</a></li>
+					<li><strong>Deakon Watson:</strong> <a href="mailto:deakonwatson@my.unt.edu">deakonwatson@my.unt.edu</a></li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default DevelopersPage
+function DevelopersPage() {
+    return (
+        <>
+            <MainScreen>
+                <Developers />
+            </MainScreen>
+        </>
+    );
+}
+
+export default DevelopersPage;
